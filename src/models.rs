27127435b6a40fb675e0c8a868c20e6c344162ed
Kazunari Okuda <saori.cube@gmail.com>
2019-08-03 17:39:38 +0200

    Implement parse_form function

--- src/models.rs
+++ src/models.rs
@@ -1,13 +1,28 @@
+use std::collections::HashMap;
+use std::io;
+
 struct NewMessage {
     username: String,
     message: String,
 }
 
 fn parse_form(from_chunk: Chunk) -> FutureResult<NewMessage, hyper::Error> {
-    futures::future::ok(NewMessage {
-        username: String::new(),
-        message: String::new(),
-    })
+    let mut form = url::form_urlencoded::parse(form_urlencoded.as_ref());
+        into_owned()
+        .collect::<HashMap<String, String>>();
+
+    if let Some(message) = form.remove("message") {
+        let username = form.remove("username").unwrap_or(String::from("anonymous"));
+        futures::future::ok(NewMessage {
+            username: username,
+            message: message,
+        })
+    } else {
+        futures::future::err(hyper::Error::from(io::Error::new(
+            io::ErrorKind::InvalidInput,
+            "Missing field message",
+        )))
+    }
 }
 
 fn write_to_db(entry: NewMessage) -> FutureResult<i64, hyper::Error> {
