313646fbc6b0a7121cb36bfbea4a7b83b19f9507
Kazunari Okuda <saori.cube@gmail.com>
2019-08-05 12:03:13 +0200

    Move methods to main

--- src/models.rs
+++ src/models.rs
@@ -1,103 +1,10 @@
-use std::collections::HashMap;
-use std::io;
-
-#[macro_use]
-extern crate serde_json;
+use schema::messages;
 
 #[derive(Insertable, Debug)]
 #[table_name = "messages"]
 pub struct NewMessage {
     username: String,
     message: String,
-}
-
-struct TimeRange {
-    before: Option<i64>,
-    after: Option<i64>,
-}
-
-fn parse_form(from_chunk: Chunk) -> FutureResult<NewMessage, hyper::Error> {
-    let mut form = url::form_urlencoded::parse(form_urlencoded.as_ref());
-        into_owned()
-        .collect::<HashMap<String, String>>();
-
-    if let Some(message) = form.remove("message") {
-        let username = form.remove("username").unwrap_or(String::from("anonymous"));
-        futures::future::ok(NewMessage {
-            username: username,
-            message: message,
-        })
-    } else {
-        futures::future::err(hyper::Error::from(io::Error::new(
-            io::ErrorKind::InvalidInput,
-            "Missing field message",
-        )))
-    }
-}
-
-fn write_to_db(entry: NewMessage) -> FutureResult<i64, hyper::Error> {
-    futures::future::ok(0)
-}
-
-fn make_post_response(
-    result: Result<i64, hyper::Error>,
-) -> FutureResult<hyper::Response, hyper::Error> {
-    match result {
-        Ok(timestamp) => {
-            let payload = json!({"timestamp": timestamp}).to_string();
-            let response = Response::new()
-                .with_header(ContentLength(payload.len() as u64))
-                .with_header(ContentType::json())
-                .with_body(payload);
-                debug!("{:?}", response);
-                futures::future::ok(response)
-        },
-        Err(error) => make_error_response(error.description()),
-    }
-}
-
-fn make_error_response(error_message: &str) -> FutureResult<hyper::Response, hyper::Error> {
-    let payload = json!({"error": error_message}).to_string();
-    let response = Response::new()
-        .with_status(StatusCode::InternalServerError)
-        .with_header(ContentLength(payload.len() as u64))
-        .with_header(ContentType::json())
-        .with_body(payload);
-    debug!("{:?}", response);
-    futures::future::ok(response)
-}
-
-fn parse_query(query: &str) -> Result<TimeRange, String> {
-    let args = url::form_urlencoded::parse(&query.as_bytes())
-        .into_owned()
-        .collect::<HashMap<String, String>>();
-    let before = args.get("before").map(|value| value.parse::<i64>());
-    if let Some(ref result) = before {
-        if let Err(ref error) = *result {
-            return Err(format!("Error paring 'before': {}", error));
-        }
-    }
-
-    Ok(TimeRange {
-        before: before.map(|b| b.unwrap()),
-        after: after.map(|a| a.unwrap()),
-    })
-}
-
-fn make_get_response(
-    messages: Option<Vec<Message>>,
-) -> FutureResult<hyper::Response, hyper::Error> {
-    let response = match messages {
-        Some(messages) => {
-            let body = render_page(messages);
-            Response::new()
-                .with_header(ContentLength(body.len() as u64))
-                .with_body(body)
-        }
-        None => Response::new().with_status(StatusCode::InternalServerError),
-    };
-    debug!("{:?}", response);
-    futures::future::ok(response)
 }
 
 #[derive(Queryable, Serialize, Debug)]
